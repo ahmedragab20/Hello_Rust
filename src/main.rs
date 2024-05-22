b85439804ceff5748e13d046bd6661cfe9b5f177
ahmedragab <ahmedragab20901@gmail.com>
2024-05-22 17:44:30 +0300

    Refactor main.rs to use Vec::reverse and Vec::push for adding elements

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-mod chapter8;
-use chapter8::vectos::{vectors_sample, vectors_sample2};
 fn main() {
-    vectors_sample();
-    let mut v: Vec<i32> = vectors_sample2();
-    println!("v: {:?}", v);
-    {
-        let v_copy = &mut v;
+    let mut x = vec![1, 2, 3, 4, 5];
+    x.reverse();
+    x.push(6);
+    x.push(7);
+    x.push(8);
+    x.push(9);
+    x.push(10);
 
-        v_copy.push(6);
-        println!("v_copy: {:?}", v_copy);
-    }
-
-    for i in &mut v {
-        *i += 50;
-        println!("i: {}", i);
-    }
-
-    println!("v_2: {:?}", v);
-
-    chapter8::hash_map::hash_map_sample();
+    println!("The value of the last element is: {:?}", x);
 }
