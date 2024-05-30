03c156bdf2196b954d518618482169d98ced2d3b
ahmedragab <ahmedragab20901@gmail.com>
2024-05-30 22:52:02 +0300

    Refactor main.rs to improve code readability and remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::fs::File;
 fn main() {
-    File::open("hello.txt").expect("Failed to open hello.txt");
-    println!("Hello, world!");
+    println!("hello world!!");
+    println!("hello world!!!!");
 }
