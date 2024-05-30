f1c8ce8e6236c76c2a21b71da420435f47eac295
ahmedragab <ahmedragab20901@gmail.com>
2024-05-30 06:59:10 +0300

    Refactor main.rs to use File::open for opening a file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
+use std::fs::File;
 fn main() {
-    let mut x = vec![1, 2, 3, 4, 5];
-    x.reverse();
-    x.push(6);
-    x.push(7);
-    x.push(8);
-    x.push(9);
-    x.push(10);
-
-    println!("The value of the last element is: {:?}", x);
+    File::open("hello.txt").expect("Failed to open hello.txt");
+    println!("Hello, world!");
 }
