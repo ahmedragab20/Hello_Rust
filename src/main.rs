c9fa0a621a3ae7156caf08c696f46bae14198bf0
ahmedragab <ahmedragab20901@gmail.com>
2024-06-22 19:01:59 +0300

    back on rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
-    println!("hello world!!");
-    println!("hello world!!!!");
+    let numbers = vec![1, 2, 365, 645, 324, 21321, 645];
+
+    for number in numbers {
+        println!("number: {number}")
+    }
+
+    println!("we're done ✅")
 }
