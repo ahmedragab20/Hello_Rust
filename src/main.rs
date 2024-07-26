ada2163de16a27cd22249307539c347d28d1ac96
ahmedragab <ahmedragab20901@gmail.com>
2024-07-26 19:16:38 +0300

    rambling with Rust

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
-use std::ops::Deref;
-
-struct SBox<T>(T);
-
-impl<T> Deref for SBox<T> {
-    type Target = T;
-    fn deref(&self) -> &T {
-        // self -> i32 -> 4 -> let num
-        &self.0
-    }
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-fn say_hi(name: &str) {
-    println!("Hi, {name}");
-}
-
+use std::{rc::Rc, thread::spawn};
+use List::{Cons, Nil};
 fn main() {
-    // let x = 4;
-    // let y = Box::new(x);
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("1) count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("2) count after creating a = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("3) count after creating a = {}", Rc::strong_count(&a));
+    }
 
-    // let num = SBox(x);
+    println!(
+        "4) count after c goes out of scope = {}",
+        Rc::strong_count(&a)
+    );
 
-    // assert_eq!(4, x);
-    // assert_eq!(4, *num);
 
-    let name: SBox<&str> = SBox("Ahmed");
-    say_hi(&name)
+    // interior mutability: Mutable borrow to immutable value.
 }
