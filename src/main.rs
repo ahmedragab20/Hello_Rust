f4a3cc9da75a3c5536b82507c2bb00f50e34a309
Ahmed Ragab <ahmedragab20901@gmail.com>
2024-10-05 19:29:42 +0300

    just playing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
+fn main() {
+    let x: &i32 = &get_number();
+    println!("x: {}", x);
 }
 
-use std::{rc::Rc, thread::spawn};
-use List::{Cons, Nil};
-fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("1) count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("2) count after creating a = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("3) count after creating a = {}", Rc::strong_count(&a));
-    }
-
-    println!(
-        "4) count after c goes out of scope = {}",
-        Rc::strong_count(&a)
-    );
-
-
-    // interior mutability: Mutable borrow to immutable value.
+fn get_number() -> i32 {
+    let x: i32 = 10;
+    x
 }
